115da3fce04d3230b710b4f6d8d25f490ab0a2e7
Bartek Łuczak <b.luczak0@gmail.com>
2022-01-02 22:46:58 +0100

    Add: login form, login/logout/register button in navbar

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 def register(request):
@@ -9,5 +11,26 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Konto zostało utworzone! Witaj ' + user)
+            return redirect ('login')
   
     return render(request, 'users/register.html', {'form': form})
+
+def loginUser(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Podany login lub hasło są nieprawidłowe')
+    context = {}
+    return render(request, 'users/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
